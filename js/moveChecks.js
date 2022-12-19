b2f164f423192c08166c9d44bf7e762eadf4d767
walleraa <walleraa@umich.edu>
2022-12-19 16:12:35 -0500

    Moved move checks to new moveChecks file

---
+++ js/moveChecks.js
@@ -0,0 +1,97 @@
+function checkLeft(step, top) {
+    const bottom = top + charHeight;
+    const right = step + charWidth;
+    let limit = 0;
+    let blockBottom = 0;
+    let blockTop = 0;
+    if (step < 0)
+        return false;
+    for (let i = 0; i < blocks.length; ++ i) {
+        limit = blocks[i][3] + blocks[i][0];
+        limit *= percentX;
+        blockBottom = blocks[i][2] + blocks[i][1];
+        blockBottom *= percentY;
+        blockTop = blocks[i][2] * percentY;
+        if (top <= blockBottom && bottom <= blockBottom && top >= blockTop && bottom >= blockTop)
+            return step >= limit || right <= limit;
+        else if (top <= blockBottom && bottom >= blockBottom)
+            return step >= limit || right <= limit;
+        else if (bottom >= blockTop && top <= blockTop)
+            return step >= limit || right <= limit;
+    }//end for
+    return true;
+}//end checkLeft
+
+function checkRight(step, top) {
+    const bottom = top + charHeight;
+    const left = step;
+    let limit = 0;
+    let blockBottom = 0;
+    let blockTop = 0;
+    step = step + charWidth;
+    if (step > width)
+        return false;
+    for (let i = 0; i < blocks.length; ++ i) {
+        limit = blocks[i][3];
+        limit *= percentX;
+        blockBottom = blocks[i][2] + blocks[i][1];
+        blockBottom *= percentY;
+        blockTop = blocks[i][2] * percentY;
+        if (top <= blockBottom && bottom <= blockBottom && top >= blockTop && bottom >= blockTop)
+            return step <= limit || left >= limit;
+        else if (top <= blockBottom && bottom >= blockBottom)
+            return step <= limit || left >= limit;
+        else if (bottom >= blockTop && top <= blockTop)
+            return step <= limit || left >= limit;
+    }//end for
+    return true;
+}//end checkRight
+
+function checkUp (step, left) {
+    const right = left + charWidth;
+    const bottom = step + charHeight;
+    let limit = 0;
+    let blockRight = 0;
+    let blockLeft = 0;
+    if (step < 0)
+        return false;
+    for (let i = 0; i < blocks.length; ++ i) {
+        limit = blocks[i][2] + blocks[i][1];
+        limit *= percentY;
+        blockRight = blocks[i][3] + blocks[i][0];
+        blockRight *= percentX;
+        blockLeft = blocks[i][3] * percentX;
+        if (left <= blockRight && right <= blockRight && left >= blockLeft && right >= blockLeft)
+            return step >= limit || bottom <= limit;
+        else if (left <= blockRight && right >= blockRight)
+            return step >= limit || bottom <= limit;
+        else if (right >= blockLeft && left <= blockLeft)
+            return step >= limit || bottom <= limit;
+    }//end for
+    return true;
+}
+
+function checkDown (step, left) {
+    const right = left + charWidth;
+    const top = step;
+    let limit = 0;
+    let blockRight = 0;
+    let blockLeft = 0;
+    step = step + charHeight;
+    if (step > height)
+        return false;
+    for (let i = 0; i < blocks.length; ++ i) {
+        limit = blocks[i][2];
+        limit *= percentY;
+        blockRight = blocks[i][3] + blocks[i][0];
+        blockRight *= percentX;
+        blockLeft = blocks[i][3] * percentX;
+        if (left <= blockRight && right <= blockRight && left >= blockLeft && right >= blockLeft)
+            return step <= limit || top >= limit;
+        else if (left <= blockRight && right >= blockRight)
+            return step <= limit || top >= limit;
+        else if (right >= blockLeft && left <= blockLeft)
+            return step <= limit || top >= limit;
+    }//end for
+    return true;
+}
